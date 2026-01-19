e931b829e7d87175b454196e2c81371de47b3a67
Upmanyu, Skand <Upmanyu.Skand+bcgprod@bcg.com>
2026-01-19 17:26:58 -0500

    Reduce padding around screen container

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export default function App() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900 p-4">
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 p-1">
       <div 
         style={{
           width: `${dimensions.width}px`,
